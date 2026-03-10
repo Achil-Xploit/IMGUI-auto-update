288ad0c4403e33728f808489c7df357973724924
Achil-ploit <63971087+Achil-Xploit@users.noreply.github.com>
2026-03-10 16:52:57 +0700

    Duplicate source files in Android.mk
    Duplicated source files in LOCAL_SRC_FILES for consistency.

--- module/src/main/cpp/Android.mk
+++ module/src/main/cpp/Android.mk
@@ -9,23 +9,23 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes/Dobby
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/ImGui/backends
 
 # --- 2. DAFTARKAN SEMUA FILE .cpp KAMU ---
 LOCAL_SRC_FILES := main.cpp \
-				   KittyMemory/KittyMemory.cpp \
-				   KittyMemory/MemoryPatch.cpp \
-				   KittyMemory/MemoryBackup.cpp \
-				   KittyMemory/KittyUtils.cpp \
-				   ImGui/backends/imgui_impl_opengl3.cpp \
-				   ImGui/backends/imgui_impl_android.cpp \
-				   ImGui/imgui.cpp \
-				   ImGui/imgui_demo.cpp \
-				   ImGui/imgui_draw.cpp \
-				   ImGui/imgui_tables.cpp \
-				   ImGui/imgui_widgets.cpp \
+                   KittyMemory/KittyMemory.cpp \
+                   KittyMemory/MemoryPatch.cpp \
+                   KittyMemory/MemoryBackup.cpp \
+                   KittyMemory/KittyUtils.cpp \
+                   ImGui/imgui.cpp \
+                   ImGui/imgui_demo.cpp \
+                   ImGui/imgui_draw.cpp \
+                   ImGui/imgui_tables.cpp \
+                   ImGui/imgui_widgets.cpp \
+                   ImGui/backends/imgui_impl_opengl3.cpp \
+                   ImGui/backends/imgui_impl_android.cpp
 
 # --- 3. TAMBAHKAN LIBRARY DOBBY ---
-# Pastikan file libdobby.a ada di folder Includes/Dobby/
 LOCAL_LDFLAGS += $(LOCAL_PATH)/Includes/Dobby/libdobby.a
 
 LOCAL_LDLIBS := -llog -landroid -lGLESv3 -lEGL -ldl
